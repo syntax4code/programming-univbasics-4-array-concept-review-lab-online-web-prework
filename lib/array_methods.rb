72783735242f87a9574f00dfd052f49c213571b1
Warren Loy Harper II <warrenharper2@att.net>
2019-07-22 23:47:12 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+ array.index(value_to_find)
 end
 
-def find_max_value(array)
-  # Add your solution here
+def def find_max_value(array)
+array.index.max(value_to_find)
 end
 
 def find_min_value(array)
-  # Add your solution here
+array.index.min(value_to_find)
 end
